a62db274c09b21bdba785c52db822729a255c865
ngil0001 <noahgilkey@gmail.com>
2025-09-14 12:59:17 -0400

    Add button and a text with a string reference

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -27,8 +28,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//Restructuring sample code to start the app theme alignment
-
 //This will be the main function for this app since it only consists of a button and image.
 @Composable
 @Preview
@@ -43,5 +42,13 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
 //Dice rolling button
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-
+    //create a vertical layout
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/}) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
