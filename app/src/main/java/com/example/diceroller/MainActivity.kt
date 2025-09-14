dfe74eda7e06faf3af3f88e7cd6e8968a81356b9
ngil0001 <noahgilkey@gmail.com>
2025-09-14 13:18:39 -0400

    added imageresources to display the image based on the result roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -20,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +51,16 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
 //Dice rolling button
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    //var for roll
+    var result  by remember { mutableIntStateOf(1) } //This ensures the value is not reset by the system since its Composable.
+    var imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     //Create a vertical layout
     Column (
         modifier = modifier,
@@ -58,7 +73,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         //Creates space between the two UI components
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { /*TODO*/}) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
